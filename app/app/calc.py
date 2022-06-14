c2bff96db47fd4be2c1dccc6b1d3e0ed43d75fad
Pham Trung <phamvinhnamtrung@gmail.com>
2022-06-14 15:18:31 +0700

    Configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,6 +5,7 @@ def add(x, y):
     """add x and y"""
     return x + y
 
+
 def subtract(x, y):
     """Substract y from x and return result"""
     return x - y
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,12 +7,13 @@ from psycopg2 import OperationalError as Pyscopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database"""
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
-        self.stdout.write('Waiting for database...') 
+        self.stdout.write('Waiting for database...')
         db_up = False
         while not db_up:
             try:
@@ -21,5 +22,5 @@ class Command(BaseCommand):
             except (Pyscopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-            
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
